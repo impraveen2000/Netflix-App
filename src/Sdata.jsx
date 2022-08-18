db91785eee90bf4dec585309a09862820723c156
praveen <impraveen2000@gmail.com>
2022-08-18 16:53:31 +0530

    intial

---
+++ src/Sdata.jsx
@@ -0,0 +1,41 @@
+const Sdata = [
+    {
+    imgsrc: "https://wallpapercave.com/wp/wp4056410.jpg" ,
+    title: "A Netflix original Series" ,
+    sname: "DARK" ,
+    link: "https://www.netflix.com/in/title/80990668?source=35" 
+    },
+    {
+    imgsrc: "https://wallpapercave.com/wp/wp7418473.jpg" ,
+    title: "A Netflix original Series" ,
+    sname: "EXTRACURRICULAR" ,
+    link: "https://www.netflix.com/in/title/80990668?source=35"
+    },
+    {
+        imgsrc: "https://wallpapercave.com/wp/wp1839578.jpg" ,
+        title: "A Netflix original Series" ,
+        sname: "STRANGER THINGS" ,
+        link: "https://www.netflix.com/in/title/80057281"
+    },
+    {
+        imgsrc: "https://wallpapercave.com/dwp1x/Bs16Cwe.jpg" ,
+        title: "A Netflix original Series" ,
+        sname: "The Vampire Diaries" ,
+        link: "https://www.netflix.com/in/title/70143860?source=35"
+
+    },
+    {
+        imgsrc: "https://wallpapercave.com/wp/wp7451970.jpg" ,
+        title: "A Netflix original Series" ,
+        sname: "My First First Love" ,
+        link: "https://www.netflix.com/in/title/81026700"
+    },
+
+
+
+
+
+
+
+];
+export default Sdata;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from 'react-dom';
+import "./index.css";
+import Card from "./Cards";
+import Sdata from "./Sdata";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+
+
+ReactDOM.render(
+  
+  <>
+  <h1 className="heading_style">List of Top 5 Series in 2020</h1>
+  {Sdata.map(function ncard(val){
+  return(
+  <Card 
+    imgsrc={val.imgsrc} 
+    title={val.title} 
+    sname={val.sname}
+    link={val.link} />
+  );
+  
+  }
+  )}
+  </>
+   
+    
+    
+    , document.getElementById("root")
+
+
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
